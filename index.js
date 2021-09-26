6583dd3312d278c9b17e947ca7a4972949e802f9
Khushal Sahni <kstheking0@gmail.com>
2021-09-26 08:38:19 +0530

    yay

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.use('/api/user', require('./user'));
 
 app.use('/api/product', require('./products'));
 
-app.listen(3000, () => console.log('App Server listening on port 3000!'));
+app.listen( process.env.PORT || 3000, () => console.log('App Server listening on port 3000!'));
